21afe1fdd9684360e6e65dd7c6e901e0444fdc35
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 04:28:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,9 +68,6 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
-
- 		  },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,11 +81,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {
-  :known_for => "Ruby",
+  programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
       :languages => ["LISP, C"]
-}
-return programmer_hash
+  }
+ programmer_hash
 
     
 end
