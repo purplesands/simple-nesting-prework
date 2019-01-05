7dfe0b7800b54869bb1df5c04789179009d8af42
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 04:28:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 #    :languages => ["LISP, C"]
 # }
 
-	programmer_hash = 
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
